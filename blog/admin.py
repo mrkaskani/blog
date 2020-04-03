9c28252fb4d855b6eeacf73283d68e5bfcbd3b95
Mohammad Reza <mrkaskani103@gmail.com>
2020-04-03 16:30:25 +0430

    modify Blog admin

--- blog/admin.py
+++ blog/admin.py
@@ -5,9 +5,15 @@ from .models import Post, Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'slug', 'author', 'published_at', 'publish', )
+    list_filter = ('title', 'body', )
+    raw_id_fields = ('author', )
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish', )
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'email', 'post', 'created_at', 'approved_comment', )
+    list_filter = ('active', 'created_at', )
+    search_fields = ('author', 'email', 'body', )
