e73ead3157a3b3541a24660a70fab52c0ffccb45
Mohammad Reza <mrkaskani103@gmail.com>
2020-04-06 11:09:19 +0430

    Debug admin model

--- blog/models.py
+++ blog/models.py
@@ -37,7 +37,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments', null=True)
-    email = models.EmailField()
+    email = models.EmailField(null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
